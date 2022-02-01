a7631b7a29545dc62fa8e48a69c5395ed8567b8a
awandahl <62021989+awandahl@users.noreply.github.com>
2022-02-01 21:52:31 +0000

    Update

--- ant.py
+++ ant.py
@@ -29,8 +29,22 @@ rpi.output(k8, 1)
 
 print("Done")
 
-@app.route('/A1_K2_A2_mcHF')
-def A1_K2_A2_mcHF():
+## all relays in parking mode which means antenna 1 connected to SDR1 and antenna 2 to SDR2
+@app.route('/')
+def parking():
+    rpi.output(k1, 1)
+    rpi.output(k2, 1)
+    rpi.output(k3, 1)
+    rpi.output(k4, 1)
+    rpi.output(k5, 1)
+    rpi.output(k6, 1)
+    rpi.output(k7, 1)
+    rpi.output(k8, 1)
+    return render_template('main.html', parking='<img src="static/parking64.png" style="vertical-align: middle">')
+
+## antennas connected to transceivers according to scheme 1
+@app.route('/1')
+def scheme_1():
     rpi.output(k1, 1)
     rpi.output(k2, 0)
     rpi.output(k3, 1)
@@ -39,10 +53,11 @@ def A1_K2_A2_mcHF():
     rpi.output(k6, 1)
     rpi.output(k7, 0)
     rpi.output(k8, 1)
-    return render_template('main.html')
+    return render_template('main.html', active_1='<img src="static/antenna64.png" style="vertical-align: middle">')
 
-@app.route('/A1_mcHF_A2_K2')
-def A1_mcHF_A2_K2():
+## antennas connected to transceivers according to scheme 2
+@app.route('/2')
+def scheme_2():
     rpi.output(k1, 0)
     rpi.output(k2, 1)
     rpi.output(k3, 1)
@@ -51,10 +66,48 @@ def A1_mcHF_A2_K2():
     rpi.output(k6, 0)
     rpi.output(k7, 1)
     rpi.output(k8, 0)
-    return render_template('main.html', utc_dt=datetime.datetime.utcnow())
+    return render_template('main.html', active_2='<img src="static/antenna64.png" style="vertical-align: middle">')
+
+## antennas connected to transceivers according to scheme 3
+@app.route('/3')
+def scheme_3():
+    rpi.output(k1, 0)
+    rpi.output(k2, 1)
+    rpi.output(k3, 1)
+    rpi.output(k4, 1)
+    rpi.output(k5, 1)
+    rpi.output(k6, 0)
+    rpi.output(k7, 1)
+    rpi.output(k8, 1)
+    return render_template('main.html', active_3='<img src="static/antenna64.png" style="vertical-align: middle">')
+
+## antennas connected to transceivers according to scheme 4
+@app.route('/4')
+def scheme_4():
+    rpi.output(k1, 0)
+    rpi.output(k2, 1)
+    rpi.output(k3, 0)
+    rpi.output(k4, 0)
+    rpi.output(k5, 1)
+    rpi.output(k6, 1)
+    rpi.output(k7, 1)
+    rpi.output(k8, 0)
+    return render_template('main.html', active_4='<img src="static/antenna64.png" style="vertical-align: middle">')
+
+## antennas connected to transceivers according to scheme 5
+@app.route('/5')
+def scheme_5():
+    rpi.output(k1, 0)
+    rpi.output(k2, 0)
+    rpi.output(k3, 0)
+    rpi.output(k4, 0)
+    rpi.output(k5, 0)
+    rpi.output(k6, 0)
+    rpi.output(k7, 0)
+    rpi.output(k8, 0)
+    return render_template('main.html', active_5='<img src="static/antenna64.png" style="vertical-align: middle">')
+
 
 if __name__=="__main__":
     print("Start")
-    app.run(debug=True, host='192.168.1.39')
-
-
+    app.run(debug=True, host='192.168.1.39', port=5000)
